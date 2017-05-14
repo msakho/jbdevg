7fb6695bdb45784ed8f638b8118de6f001f9e1c9
enonowog <nelvadas@gmail.com>
2017-05-14 05:44:12 +0200

    Virtual procedure

--- jbdatavirt/beosbank-datavirt/SQLScrapbook0.sql
+++ jbdatavirt/beosbank-datavirt/SQLScrapbook0.sql
@@ -3,3 +3,10 @@
 
 SELECT * FROM Transactions.MoneyTransfer where code in ('BEUK001','FRCM001') ;
 SELECT * FROM AF_Transactions.AF_Customer;
+
+//Virtual procedure TEST
+EXEC EMEA_REF_VBL.getTransferFees(50.00,'FRANCE');
+
+SELECT feesRate,name FROM EMEA_REF_VBL.countries
+
+SELECT feesRate FROM EMEA_REF_VBL.countries WHERE name = 'FRANCE'
