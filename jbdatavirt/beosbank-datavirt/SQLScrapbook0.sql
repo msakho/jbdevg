e3b1c56dd39bdde5b038cee48f3a316b9e047d46
enonowog <enonowog@localhost>
2017-05-14 03:25:41 +0200

    Models metadata visibility

--- jbdatavirt/beosbank-datavirt/SQLScrapbook0.sql
+++ jbdatavirt/beosbank-datavirt/SQLScrapbook0.sql
@@ -1,4 +1,5 @@
 // 1) Enter valid SQL query (i.e. SELECT * FROM FOO)
 // 2) Highlight query and right-click select "Execute Selected Text"
 
-SELECT * FROM Transactions.MoneyTransfer where code in ('BEUK001','FRCM001') 
+SELECT * FROM Transactions.MoneyTransfer where code in ('BEUK001','FRCM001') ;
+SELECT * FROM AF_Transactions.AF_Customer;
