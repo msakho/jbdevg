011700cc7067d780e5b6d4956678087e95b5a2d3
enonowog <nelvadas@gmail.com>
2017-05-31 00:57:38 +0200

    Fabric8 deployments

--- jbfuse/beosbank-moneytransfert-cacheloader/src/main/java/com/beosbank/jbdevg/jbfuse/cacheloader/processor/AFProcessor.java
+++ jbfuse/beosbank-moneytransfert-cacheloader/src/main/java/com/beosbank/jbdevg/jbfuse/cacheloader/processor/AFProcessor.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import com.beosbank.jbdevg.jbfuse.cacheloader.model.MoneyTransfer;
 
+@SuppressWarnings("unchecked")
 public class AFProcessor implements  Processor {
 	
 	Logger logger = LoggerFactory.getLogger(AFProcessor.class);
@@ -22,18 +23,13 @@ public class AFProcessor implements  Processor {
 	public void process(Exchange exchange) throws Exception {
 		
 		Message in = exchange.getIn();
-		
-		@SuppressWarnings("unchecked")
 		Map<String,Object> tuple= (Map<String, Object>) in.getBody();
-		
 		MoneyTransfer mt = new  MoneyTransfer();
 		mt.setId((Long)tuple.get(MoneyTransfer.FIELD_ID));
 		mt.setKeycode((String)tuple.get(MoneyTransfer.FIELD_KEYCODE));
 		mt.setPaymentMethod((String)tuple.get(MoneyTransfer.FIELD_PAYMENT_METHOD));
 		mt.setAmount_hf_sender_currency((BigDecimal)tuple.get(MoneyTransfer.FIELD_AMOUNT_HF_SENDER_CUR));
 		mt.setStatus((String)tuple.get(MoneyTransfer.FIELD_STATUS));
-		
-		
 		in.setBody(mt);
 		in.setHeader(InfinispanConstants.KEY, mt.getKeycode());
 	
